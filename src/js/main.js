60cf25126ab05610b6e3db22db7d9f05ee3e3309
Artem <artem.rassadin.05@mail.ru>
2025-04-11 14:17:39 +0300

    Ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import './components/slider.js'
+import './components/slider.js';
 
 // dropdown
 
@@ -11,10 +11,12 @@ function activateDropdownMenu() {
             const dropdownMenu = item.querySelector('.dropdown-menu');
 
             item.addEventListener('mouseover', (e) => {
-                if (e.target.classList.contains('nav-link')) {
-                    dropdownMenu.classList.add('active');
-                    headerWrapper.classList.add('active');
-                }
+                dropdowns.forEach(elem => {
+                    elem.querySelector('.dropdown-menu').classList.remove('active')
+                })
+
+                dropdownMenu.classList.add('active');
+                headerWrapper.classList.add('active');
             })
 
             item.addEventListener('mouseleave', () => {
@@ -83,7 +85,7 @@ catalogueOverlay.addEventListener('mouseleave', () => {
 
 // catalogue overlay mobile
 
-const mobileTitle = document.querySelector('.mobile-menu__title')
+const mobileTitle = document.querySelector('.mobile-catalogue__title')
 const attrLayouts = document.querySelectorAll("[data-layout]")
 const initialMenu = document.querySelectorAll('[data-initial-layout]')
 const mobileMenu = {}
@@ -110,8 +112,6 @@ attrLayouts.forEach(item => {
         mobileMenu[`${+layouts[headerPosition]}`] = [itemTitle]
     }
 })
-
-console.log(mobileMenu)
 
 // на второй итерации составляем меню
 attrLayouts.forEach(item => {
@@ -141,6 +141,108 @@ attrLayouts.forEach(item => {
     }
 })
 
+// click catalogue mobile
+
+function menuGenerator(elements, mainMenuField) {
+    mobileTitle.innerHTML = elements[0]
+    mainMenuField.innerHTML = ''
+
+    for (let i = 1; i < elements.length; i++) {
+        if (elements[i].length === 2) {
+            mainMenuField.insertAdjacentHTML(`beforeend`, `
+                <li class="header-menu__item nav-item menu__dropdown position-relative" data-layout="${elements[i][1]}">
+                    <span class="nav-link py-lg-2 py-3 d-lg-block d-flex justify-content-between align-items-center">
+                        <span>${elements[i][0]}</span>
+                        <svg class="small-arrow d-lg-none d-block">
+                            <use xlink:href="./img/svg/sprite.svg#small-arrow"></use>
+                        </svg>
+                    </span>
+                </li>
+            `)
+        } else {
+            mainMenuField.insertAdjacentHTML(`beforeend`, `
+                <li class="nav-item">
+                    <a href="#" class="py-lg-3 py-2 d-block"><span>${elements[i][0]}</span></a>
+                </li>
+            `)
+        }
+    }
+}
+
+// составляем само меню при нажатии на кнопки
+
+let m_history = [];
+const arrowBack = document.querySelector('.mobile-catalogue-arrow')
+const overlayMenu = document.querySelector('.overlay-menu')
+const mobileCatalogue = document.querySelector('.mobile-catalogue__content');
+
+function computeMobileMenu() {
+    const catalogueItem = document.querySelector(".mobile-catalogue")
+    const dataLayout = document.querySelectorAll('[data-layout]');
+
+    catalogueItem.addEventListener('click', () => {
+        overlayMenu.classList.add('d-none')
+        mobileCatalogue.classList.remove('d-none')
+
+        m_history.push(0)
+
+        let elements;
+
+        if (!m_history.length) {
+            arrowBack.classList.remove('active')
+        } else {
+            arrowBack.classList.add('active')
+            elements = mobileMenu[m_history.at(-1)];
+        }
+
+        menuGenerator(elements, mobileCatalogue.querySelector('.header-menu'))
+        computeMobileMenu();
+    })
+
+    dataLayout.forEach(item => {
+        item.addEventListener('click', () => {
+            mobileCatalogue.querySelector('.header-menu').innerHTML = ''
+
+            m_history.push(+item.dataset.layout)
+
+            let elements;
+
+            if (!m_history.length) {
+                arrowBack.classList.remove('active')
+            } else {
+                arrowBack.classList.add('active')
+                elements = mobileMenu[m_history.at(-1)];
+            }
+
+            menuGenerator(elements, mobileCatalogue.querySelector('.header-menu'))
+            computeMobileMenu();
+        })
+    })
+}
+
+arrowBack.addEventListener('click', () => {
+    if (m_history.length) {
+        m_history.pop();
+
+        let elements;
+
+        console.log(m_history)
+
+        if (!m_history.length) {
+            arrowBack.classList.remove('active')
+            overlayMenu.classList.remove('d-none')
+            mobileCatalogue.classList.add('d-none')
+        } else {
+            elements = mobileMenu[m_history.at(-1)];
+        }
+
+        menuGenerator(elements, mobileCatalogue.querySelector('.header-menu'))
+        computeMobileMenu();
+    }
+})
+
+computeMobileMenu();
+
 // search
 
 const searchButton = document.querySelector('.header__mobile-icons .search');
@@ -150,23 +252,32 @@ const crossSearch = document.querySelector('.cross-search');
 
 searchButton.addEventListener('click', () => {
     searchOverlay.classList.toggle('active');
-    backgroundPopup.classList.toggle('active');
+
+    if (searchOverlay.classList.contains('active')) {
+        backgroundPopup.classList.add('active');
+    } else {
+        backgroundPopup.classList.remove('active');
+    }
+
+    document.body.classList.toggle('overflow-hidden')
 
     document.querySelector('.header-menu-wrapper').classList.remove('active');
     document.querySelector('.overlay-contacts').classList.remove('active')
+    document.querySelector('.overlay-place').classList.remove('active')
 
-    if (headerWrapper.classList.contains('active')) {
+    if (searchOverlay.classList.contains('active')) {
+        headerWrapper.classList.add('active')
+    } else {
         setTimeout(() => {
             headerWrapper.classList.remove('active')
         }, 300)
-    } else {
-        headerWrapper.classList.toggle('active')
     }
 })
 
 crossSearch.addEventListener('click', () => {
     searchOverlay.classList.remove('active');
     backgroundPopup.classList.remove('active');
+    document.body.classList.remove('overflow-hidden')
 
     setTimeout(() => {
         headerWrapper.classList.remove('active')
@@ -181,22 +292,33 @@ const crossContact = document.querySelector('.cross-contact');
 
 contactsButton.addEventListener('click', () => {
     contactsOverlay.classList.toggle('active');
-    backgroundPopup.classList.toggle('active');
+    document.body.classList.toggle('overflow-hidden')
+
+    if (contactsOverlay.classList.contains('active')) {
+        backgroundPopup.classList.add('active');
+    } else {
+        backgroundPopup.classList.remove('active');
+    }
+
     document.querySelector('.header-menu-wrapper').classList.remove('active');
     searchOverlay.classList.remove('active')
 
-    if (headerWrapper.classList.contains('active')) {
+    document.querySelector('.header-menu-wrapper').classList.remove('active');
+    document.querySelector('.overlay-place').classList.remove('active')
+
+    if (contactsOverlay.classList.contains('active')) {
+        headerWrapper.classList.add('active')
+    } else {
         setTimeout(() => {
             headerWrapper.classList.remove('active')
         }, 300)
-    } else {
-        headerWrapper.classList.toggle('active')
     }
 })
 
 crossContact.addEventListener('click', () => {
     contactsOverlay.classList.remove('active');
     backgroundPopup.classList.remove('active');
+    document.body.classList.remove('overflow-hidden')
 
     setTimeout(() => {
         headerWrapper.classList.remove('active')
@@ -210,22 +332,34 @@ const crossPlace = placeOverlay.querySelector('.cross-place');
 
 placeButton.addEventListener('click', () => {
     placeOverlay.classList.toggle('active');
-    backgroundPopup.classList.toggle('active');
+    document.body.classList.toggle('overflow-hidden')
+
+    if (placeOverlay.classList.contains('active')) {
+        backgroundPopup.classList.add('active');
+    } else {
+        backgroundPopup.classList.remove('active');
+    }
+
     document.querySelector('.header-menu-wrapper').classList.remove('active');
     searchOverlay.classList.remove('active')
 
-    if (headerWrapper.classList.contains('active')) {
+    document.querySelector('.header-menu-wrapper').classList.remove('active');
+    document.querySelector('.overlay-contacts').classList.remove('active')
+    document.querySelector('.overlay-search').classList.remove('active')
+
+    if (placeOverlay.classList.contains('active')) {
+        headerWrapper.classList.add('active')
+    } else {
         setTimeout(() => {
             headerWrapper.classList.remove('active')
         }, 300)
-    } else {
-        headerWrapper.classList.toggle('active')
     }
 })
 
 crossPlace.addEventListener('click', () => {
     placeOverlay.classList.remove('active');
     backgroundPopup.classList.remove('active');
+    document.body.classList.remove('overflow-hidden')
 
     setTimeout(() => {
         headerWrapper.classList.remove('active')
@@ -266,11 +400,13 @@ const cityCross = cityPopup.querySelector('.cross-place')
 headerCity.addEventListener('click', () => {
     cityPopup.classList.add('active')
     contactsBlackBackground.classList.add('active')
+    document.body.classList.add('overflow-hidden')
 })
 
 cityCross.addEventListener('click', () => {
     cityPopup.classList.remove('active')
     contactsBlackBackground.classList.remove('active')
+    document.body.classList.remove('overflow-hidden')
 })
 
 contactsBlackBackground.addEventListener('click', () => {
@@ -279,6 +415,7 @@ contactsBlackBackground.addEventListener('click', () => {
     extraPhone.classList.remove('active')
     cityPopup.classList.remove('active')
     arrowExtra.classList.remove('active')
+    document.body.classList.remove('overflow-hidden')
 })
 
 // menu-burger
@@ -289,10 +426,18 @@ const crossMenu = document.querySelector('.cross-menu')
 
 burger.addEventListener('click', () => {
     menuOverlay.classList.toggle('active');
+
+    backgroundPopup.classList.remove('active');
+    searchOverlay.classList.remove('active');
+    contactsOverlay.classList.remove('active');
+    placeOverlay.classList.remove('active');
+
+    document.body.classList.add('overflow-hidden')
 })
 
 crossMenu.addEventListener('click', () => {
     menuOverlay.classList.remove('active');
+    document.body.classList.remove('overflow-hidden')
 
     setTimeout(() => {
         resetDropdown()
@@ -301,10 +446,14 @@ crossMenu.addEventListener('click', () => {
 
 // reset overlays on click on backgroundPopup
 backgroundPopup.addEventListener('click', () => {
-    searchOverlay.classList.remove('active');
     backgroundPopup.classList.remove('active');
+
+    searchOverlay.classList.remove('active');
     contactsOverlay.classList.remove('active');
+    placeOverlay.classList.remove('active');
     menuOverlay.classList.remove('active');
+
+    document.body.classList.remove('overflow-hidden')
 
     setTimeout(() => {
         headerWrapper.classList.remove('active')
@@ -402,3 +551,62 @@ countryBlocks.forEach(item => {
         }
     })
 })
+
+// footer accordeon
+
+const itemFooter = document.querySelectorAll('.item-footer')
+
+itemFooter.forEach(item => {
+    item.querySelector('.item-footer__title').addEventListener('click', () => {
+        const list = item.querySelector('ul');
+        let heightCounter = 0;
+
+        if (item.classList.contains('active')) {
+            list.style.maxHeight = 0;
+        } else {
+            list.querySelectorAll('li').forEach(elem => heightCounter += elem.clientHeight)
+            heightCounter += (list.querySelectorAll('li').length - 1) * 8
+            list.style.maxHeight = heightCounter + 'px'
+        }
+
+        item.classList.toggle('active')
+        item.querySelector('.item-footer__title').classList.toggle('active')
+        item.querySelector('.arrow').classList.toggle('active')
+        item.querySelector('ul').classList.toggle('active')
+    })
+})
+
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
+lightGallery(document.getElementById('animated-thumbnails'), {
+    thumbnail: true,
+    selector: '.blagodar-image__wrapper',
+});
+
+for (let i = 1; i <= 4; i++) {
+    lightGallery(document.getElementById(`open-video-${i}`), {
+        selector: 'this',
+        iframeMaxHeight: '90%'
+    });
+}
+
+// fix header on mobile
+
+const mainSlider = document.querySelector('.main-slider')
+const headers = document.querySelector('.headers')
+
+if (document.body.clientWidth <= 992) {
+    mainSlider.style.paddingTop = headers.clientHeight + 'px';
+}
+
+window.onresize = () => {
+    if (document.body.clientWidth <= 992) {
+        mainSlider.style.paddingTop = headers.clientHeight + 'px';
+    }
+}
